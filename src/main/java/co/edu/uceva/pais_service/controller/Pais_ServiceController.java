bf801cf9dc32ec24e3e3151226af563d7e928c6f
Yilver Rivera <yilver.rivera01@uceva.edu.co>
2023-03-08 08:31:31 -0500

    Creación del proyecto con Springboot.

---
+++ src/main/java/co/edu/uceva/pais_service/controller/Pais_ServiceController.java
@@ -0,0 +1,15 @@
+package co.edu.uceva.pais_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/Pais-service")
+public class Pais_ServiceController {
+    @GetMapping("/bienvenido")
+    //Método que da la bienvenida al navegador.
+    public String imprimir(){ //TODO Saludo con nombre
+        return "Hola. Bienvenido a Pais Service.";
+    }
+}
